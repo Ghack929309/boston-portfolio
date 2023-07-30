0a119e95c3e46c0b0a751b180297ce2c4e0c5dcd
Ghack929309 <jcalixte024@gmail.com>
2023-07-30 14:13:57 -0400

    added overflow to mockup, hided scrollbar

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -41,10 +41,8 @@ export default function ProjectKey() {
   if (!figmaData) return <div className="min-h-screen"></div>;
   return (
     <div className=" relative flex flex-col items-center bg-accent/10 justify-center min-h-screen">
-      <div className="absolute left-10 top-0">
-        <label
-          onClick={toggleMode}
-          className="relative inline-flex items-center mr-5 cursor-pointer">
+      <div className="absolute right-0 top-0">
+        <label className="relative inline-flex items-center mr-5 cursor-pointer">
           <input
             type="checkbox"
             className="sr-only peer"
@@ -53,7 +51,7 @@ export default function ProjectKey() {
           />
           <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary"></div>
           <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-            Dark mode
+            {darkMode ? "Dark" : "Light"}
           </span>
         </label>
       </div>
@@ -87,18 +85,20 @@ export default function ProjectKey() {
                 <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[124px] rounded-l-lg"></div>
                 <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[178px] rounded-l-lg"></div>
                 <div className="h-[64px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -right-[17px] top-[142px] rounded-r-lg"></div>
-                <div className="rounded-[2rem] overflow-auto w-[272px] h-[572px] bg-white dark:bg-gray-800">
-                  <Image
-                    priority
-                    quality={100}
-                    src={item.images}
-                    placeholder="blur"
-                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
-                    alt="Picture of the author"
-                    width={272}
-                    height={500}
-                    className="object-cover h-[99.99%]"
-                  />
+                <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white dark:bg-gray-800">
+                  <div className="overflow-y-auto max-h-[572px]">
+                    <Image
+                      priority
+                      quality={100}
+                      src={item.images}
+                      placeholder="blur"
+                      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+                      alt="Picture of the author"
+                      width={272}
+                      height={500}
+                      className="object-cover h-full"
+                    />
+                  </div>
                   {/* <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mockup-1-light.png" className="dark:hidden w-[272px] h-[572px]" alt="">
         <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mockup-1-dark.png" className="hidden dark:block w-[272px] h-[572px]" alt=""> */}
                 </div>
