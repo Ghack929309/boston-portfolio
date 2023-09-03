4c4ec302278ff0890a372d6aa9286aa1ece34194
Ghack929309 <jcalixte024@gmail.com>
2023-09-02 20:59:00 -0400

    fix styling

--- src/components/PdfViewer/PdfViewer.tsx
+++ src/components/PdfViewer/PdfViewer.tsx
@@ -7,15 +7,13 @@ type PdfViewerType = {
 export const PdfViewer = ({ docs }: PdfViewerType) => {
 	if (!docs) return null;
 	return (
-		<div className="w-full h-full">
-			<iframe
-				src={docs[0].uri}
-				width="100%"
-				height="100vh"
-				allow="autoplay"
-				allowFullScreen
-				style={{ border: "none" }}
-			></iframe>
-		</div>
+		<iframe
+			src={docs[0].uri}
+			width="100%"
+			height="100vh"
+			allow="autoplay"
+			allowFullScreen
+			style={{ border: "none" }}
+		></iframe>
 	);
 };
