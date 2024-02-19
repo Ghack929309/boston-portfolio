e3941d1ab72741f3046fdd78fc8cb12afe7a4aca
Ghack929309 <jcalixte024@gmail.com>
2024-02-18 19:35:05 -0500

    fixed card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { media } from "@wix/api-client";
 import Image from "next/image";
-import React, { ReactNode } from "react";
+import React from "react";
 
 type CardProps = {
   image: string;
@@ -17,19 +17,19 @@ export default function Card({ image, title, subTitle, id }: CardProps) {
     <Link
       prefetch
       href={`/project/${id}`}
-      className="flex flex-col items-center max-w-[420px] overflow-hidden rounded-[40px] hover:shadow-lg hover:shadow-indigo-300 py-2 ">
+      className="flex flex-col items-center w-auto overflow-hidden rounded-[40px] hover:shadow-lg hover:shadow-indigo-300 py-2 ">
       <Image
-        className="object-cover w-auto h-auto "
+        className="object-cover max-w-[320px] h-auto "
         quality={100}
         sizes="100vw"
         priority
         src={imageMedia.url}
         alt={title}
-        width={420}
+        width={320}
         height={300}
       />
 
-      <h3 className="max-w-xs truncate px-4 text-zinc-800 text-center text-sm font-medium capitalize pt-2 ">
+      <h3 className="max-w-xs px-4 text-zinc-800 text-center text-sm font-medium capitalize my-2 ">
         {title}
       </h3>
     </Link>
